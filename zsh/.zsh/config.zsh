953fa8a4e00fe8fff32b2a97f2025d38ba0ff472
calmery <calmery@pixiv.com>
2019-10-07 11:14:33 +0900

    Fix config.zsh

--- zsh/.zsh/config.zsh
+++ zsh/.zsh/config.zsh
@@ -1,6 +1,9 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"
 
+eval "$(pyenv init -)"
+eval "$(rbenv init -)"
+
 export LANG=en_US.UTF-8
 
 bindkey '^[[A' history-substring-search-up
