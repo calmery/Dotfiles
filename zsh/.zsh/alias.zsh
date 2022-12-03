485089dbc30f83c999f2fd60e80c3890f80ef0c3
calmery <calmery@users.noreply.github.com>
2022-12-04 03:57:04 +0900

    Update

--- zsh/.zsh/alias.zsh
+++ zsh/.zsh/alias.zsh
@@ -1,5 +1,7 @@
 alias l='exa -la'
 alias ls='exa -lhi'
 alias b='git branch | grep "*" | cut -d " " -f2'
+alias d="git status --porcelain | sed s/^...// | sed -e :loop -e 'N; $!b loop' -e 's/\n/ /g'"
 alias m='git log -n 1 master --pretty=format:"%H"'
 alias p='git push origin `b`'
+alias npm='~/.n/bin/npm'
