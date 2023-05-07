2f3e2768721e489817491c68ebaa90d5d1413994
calmery <12670155+calmery@users.noreply.github.com>
2023-05-08 03:17:22 +0900

    Update

--- zsh/.zsh/config.zsh
+++ zsh/.zsh/config.zsh
@@ -17,29 +17,3 @@ autoload -Uz compinit
 compinit
 
 setopt correct
-
-export GOENV_ROOT="$HOME/.goenv"
-export PATH="$GOENV_ROOT/bin:$PATH"
-eval "$(goenv init -)"
-
-export PATH="/usr/local/opt/mysql@5.6/bin:$PATH"
-
-export PATH="/usr/local/opt/imagemagick@6/bin:$PATH"
-export LDFLAGS="-L/usr/local/opt/imagemagick@6/lib"
-export CPPFLAGS="-I/usr/local/opt/imagemagick@6/include"
-export PKG_CONFIG_PATH="/usr/local/opt/imagemagick@6/lib/pkgconfig"
-
-export PATH="/Applications/Blender.app/Contents/MacOS:$PATH"
-
-export LDFLAGS="-L/usr/local/opt/mysql@5.6/lib"
-export CPPFLAGS="-I/usr/local/opt/mysql@5.6/include"
-
-eval "$(exenv init -)"
-
-source "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc"
-source "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
-
-export PATH="/Users/calmery/.deno/bin:$PATH"
-
-export N_PREFIX="$HOME/.n"
-export PATH="$HOME/.n/bin:$PATH"
