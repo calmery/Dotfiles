997ef3768671207243ebe5a922e6e88f48384c9b
calmery <12670155+calmery@users.noreply.github.com>
2023-05-08 03:35:22 +0900

    Update

---
+++ volume-adjuster/index.js
@@ -0,0 +1,36 @@
+const { setInterval } = require("timers/promises");
+const {
+  getDefaultOutputDevice,
+  getOutputDeviceVolume,
+  setOutputDeviceVolume,
+} = require("macos-audio-devices");
+const nodeNotifier = require("node-notifier");
+
+const main = async () => {
+  for await (const _ of setInterval(1000)) {
+    try {
+      const defaultOutputDevice = await getDefaultOutputDevice();
+
+      if (defaultOutputDevice.transportType === "usb") {
+        const { id } = defaultOutputDevice;
+
+        const outputDeviceVolume = await getOutputDeviceVolume(id);
+        if (outputDeviceVolume > 0.5) {
+          await setOutputDeviceVolume(id, 0.5);
+
+          nodeNotifier.notify({
+            title: "Volume Adjuster",
+            message: "Volume has been automatically adjusted.",
+          });
+        }
+      }
+    } catch(error) {
+      nodeNotifier.notify({
+        title: "Volume Adjuster",
+        message: error.message,
+      });
+    }
+  }
+};
+
+main();
