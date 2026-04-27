a50d1cbae63b323cc8996300bb72c4663c15051a
calmery <12670155+calmery@users.noreply.github.com>
2026-04-28 01:40:02 +0900

    Update

--- zsh/.zsh/alias.zsh
+++ zsh/.zsh/alias.zsh
@@ -1,5 +1,5 @@
-alias l='exa -la'
-alias ls='exa -lhi'
+alias l='eza -la'
+alias ls='eza -lhi'
 alias b='git branch | grep "*" | cut -d " " -f2'
 alias d="git status --porcelain | sed s/^...// | sed -e :loop -e 'N; $!b loop' -e 's/\n/ /g'"
 alias m='git log -n 1 master --pretty=format:"%H"'
